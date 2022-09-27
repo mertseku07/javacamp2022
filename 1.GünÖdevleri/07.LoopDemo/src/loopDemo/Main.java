613d66d7e655b7e4189eeadc13fb907167f25568
Mert Sekü <mrtseku@gmail.com>
2022-09-27 17:51:28 +0300

    finished

--- 1.GünÖdevleri/07.LoopDemo/src/loopDemo/Main.java
+++ 1.GünÖdevleri/07.LoopDemo/src/loopDemo/Main.java
@@ -3,11 +3,19 @@ package loopDemo;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//For
 		for(int i=2; i<10; i+=2) {
 			System.out.println(i);
 		}
-		System.out.println("Döngü Bitti");
+		System.out.println("For Döngüsü Bitti");
+		
+		//While
+		int i = 2;
+		while(i < 10) {
+			System.out.println(i);
+			i+=2;
+		}
+		System.out.println("While Döngüsü Bitti");
 	}
 
 }
