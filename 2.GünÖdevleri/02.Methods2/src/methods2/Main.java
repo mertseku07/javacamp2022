6159e5593c4c597a8cdfb6244e62b9fbdd796235
Mert Sekü <mrtseku@gmail.com>
2022-10-04 14:49:25 +0300

    Finished

--- 2.GünÖdevleri/02.Methods2/src/methods2/Main.java
+++ 2.GünÖdevleri/02.Methods2/src/methods2/Main.java
@@ -8,7 +8,8 @@ public class Main {
 		System.out.println(yeniMesaj);
 		int sayi = topla(15,7);
 		System.out.println(sayi);
-		
+		int toplam = topla2(2,3,4);
+		System.out.println(toplam);
 	}
 	
 	public static void ekle () {
@@ -26,6 +27,14 @@ public class Main {
 	public static int topla(int sayi1, int sayi2) {
 		return sayi1 + sayi2;
 		
+	}
+	
+	public static int topla2(int... sayilar) {
+		int toplam = 0;
+		for(int sayi:sayilar) {
+			toplam += sayi;
+		}
+		return toplam;
 	}
 	
 	public static String sehirVer() {
